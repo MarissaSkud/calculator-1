7c23303aa7839eb020baef1a45e11cf5d0efce19
Hackbright Student <no-reply@hackbrightacademy.com>
2019-04-10 12:10:30 -0700

    Subtract & multiply positive integers.

--- arithmetic.py
+++ arithmetic.py
@@ -3,17 +3,19 @@
 
 def add(num1, num2):
     """Return the sum of the two inputs."""
-    sum = float(num1) + float(num2) 
-    return sum
+    added = num1 + num2
+    return added
 
 
 def subtract(num1, num2):
     """Return the second number subtracted from the first."""
-
+    sub = num1 - num2
+    return sub
 
 def multiply(num1, num2):
     """Multiply the two inputs together."""
-
+    product = num1 * num2
+    return product
 
 def divide(num1, num2):
     """Divide the first input by the second and return the result."""
