76c709c52f41817bb8c7a75347a78dc4fd419193
Hackbright Student <no-reply@hackbrightacademy.com>
2019-04-10 12:15:55 -0700

    Divide, square, cube, exponent & modulus for positive integers.

--- arithmetic.py
+++ arithmetic.py
@@ -19,19 +19,27 @@ def multiply(num1, num2):
 
 def divide(num1, num2):
     """Divide the first input by the second and return the result."""
-
+    divided = num1 / num2 
+    return divided 
 
 def square(num1):
     """Return the square of the input."""
+    squared = num1 * num1 
+    return squared
 
 
 def cube(num1):
     """Return the cube of the input."""
+    cubed = num1 * num1 * num1 
+    return cubed 
 
 
 def power(num1, num2):
     """Raise num1 to the power of num2 and return the value."""
-
+    powered = num1 ** num2
+    return powered
 
 def mod(num1, num2):
     """Return the remainder of num1 / num2."""
+    remainder = num1 % num2
+    return remainder
